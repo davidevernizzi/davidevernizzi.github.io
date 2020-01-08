efe45dfd8b96c6f10ed05a1de9ca47e03d627bfa
Davide Vernizzi <davide@vernizzis.it>
2020-01-08 22:31:11 +0100

    fix: pin Jekiyll version for local dev

--- serve.sh
+++ serve.sh
@@ -6,4 +6,4 @@
 # Build site to get the gallery
 # bundle exec jekyll serve
 
-docker run --rm --volume="$PWD:/srv/jekyll" --volume="$PWD/vendor/bundle:/usr/local/bundle" -p 4000:4000 -it jekyll/jekyll:$JEKYLL_VERSION jekyll serve
+docker run --rm --volume="$PWD:/srv/jekyll" --volume="$PWD/vendor/bundle:/usr/local/bundle" -p 4000:4000 -it jekyll/jekyll:3.8 jekyll serve
