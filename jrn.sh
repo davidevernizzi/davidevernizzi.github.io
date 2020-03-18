7c63936329e482874d883bd9d599d745f7f949ca
Davide Vernizzi <davide@vernizzis.it>
2020-03-18 23:26:19 +0100

    feat(): draft of journal

--- jrn.sh
+++ jrn.sh
@@ -4,6 +4,6 @@
 DATE=`date +%Y-%m-%d`
 FILENAME="$DATE-`echo $* | tr ' ' '-'`.markdown"
 
-cat _journal/template.markdown | sed -e "s/TITLE/$DATE/g" | sed -e "s/DATE/$DATE/g" > _journal/$FILENAME
+cat _journal/template| sed -e "s/TITLE/$DATE/g" | sed -e "s/DATE/$DATE/g" > _journal/$FILENAME
 
 vim _journal/$FILENAME
